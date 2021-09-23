0f6380e67d3828e3f064f8c9232d4ef793572787
YaodongLiu <ttttttliu@qq.com>
2021-09-23 14:33:56 +0800

    配置eureka-server

--- eu/src/main/java/com/bocd/lyd/eurekaserver/EurekaServerApplication.java
+++ eu/src/main/java/com/bocd/lyd/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,12 @@ package com.bocd.lyd.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author liuyaodong
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
