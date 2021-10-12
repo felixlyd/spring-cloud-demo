95730af022c5b1ed7db557ab99e1d909ffbc363d
YaodongLiu <ttttttliu@qq.com>
2021-10-12 17:22:12 +0800

    demo17：过滤器简单实现

---
+++ demo-17/README.md
@@ -0,0 +1,5 @@
+# 工程简介
+spring cloud实战派实例17：用AddRequestHeader过滤器工厂给请求
+添加Header参数
+# 延伸阅读
+
---
+++ demo-17/src/test/java/org/example/felixlyd/demo17/Demo17ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.felixlyd.demo17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
