a7d51bb3e957e02ffd1f111138f07e8a17d037ec
YaodongLiu <ttttttliu@qq.com>
2021-10-27 17:14:05 +0800

    demo21：sleuth

---
+++ demo-22-zipkin/README.md
@@ -0,0 +1,4 @@
+# 工程简介
+spring cloud实战派实例22：在SpringCloud中使用Zipkin
+# 延伸阅读
+
---
+++ demo-22-zipkin/src/main/java/org/example/felixlyd/demo22zipkin/Demo22ZipkinApplication.java
@@ -0,0 +1,17 @@
+package org.example.felixlyd.demo22zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuyaodong
+ * @date 2021/10/27
+ */
+@SpringBootApplication
+public class Demo22ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo22ZipkinApplication.class, args);
+    }
+
+}
