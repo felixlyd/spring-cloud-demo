c98b0c09c7eaed107541e17950a3b2868cbf6502
YaodongLiu <ttttttliu@qq.com>
2021-10-27 17:16:33 +0800

    demo22：Zipkin

---
+++ demo-22-zipkin/src/test/java/org/example/felixlyd/demo22zipkin/Demo22ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.felixlyd.demo22zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22ZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
