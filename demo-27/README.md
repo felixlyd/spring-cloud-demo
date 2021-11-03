92ae120cebf7058c129d35d2b64309f55e505876
YaodongLiu <ttttttliu@qq.com>
2021-11-03 09:53:16 +0800

    demo28，配置中心通过consul托管

--- demo-27/README.md
+++ demo-27/README.md
@@ -1,4 +1,5 @@
 # 工程简介
 spring cloud实战派实例27：用Spring Cloud Bus自动刷新配置信息
+spring cloud实战派实例28：实现配置中心和配置客户端的服务化
 # 延伸阅读
 
--- demo-27/demo-27-config-client/src/main/java/com/example/demo27configclient/Demo27ConfigClientApplication.java
+++ demo-27/demo-27-config-client/src/main/java/com/example/demo27configclient/Demo27ConfigClientApplication.java
@@ -2,12 +2,14 @@ package com.example.demo27configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liuyaodong
  * @date 2021/10/29
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Demo27ConfigClientApplication {
 
     public static void main(String[] args) {
