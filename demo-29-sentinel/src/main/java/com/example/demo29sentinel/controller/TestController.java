268aadfe06a76f605cd24dd52615ccd378e829ce
YaodongLiu <ttttttliu@qq.com>
2021-11-04 11:37:42 +0800

    demo31,sentinel测试熔断

--- demo-29-sentinel/src/main/java/com/example/demo29sentinel/controller/TestController.java
+++ demo-29-sentinel/src/main/java/com/example/demo29sentinel/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo29sentinel.controller;
 
+import com.alibaba.csp.sentinel.Tracer;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,15 @@ public class TestController {
     @SentinelResource("hello")
     public String hello(){
         return "Hello Sentinel Demo";
+    }
+    @GetMapping("/exception")
+    @SentinelResource("exception")
+    public String exception(){
+        try{
+            throw new RuntimeException("出错");
+        }catch (Throwable throwable){
+            Tracer.trace(throwable);
+        }
+        return "测试异常比率";
     }
 }
